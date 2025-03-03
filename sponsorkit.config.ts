e476bd57d45dd6a23464701c1f73144cdae39146
JounQin <admin@1stg.me>
2025-03-03 14:28:11 +0800

    chore: include past sponsors

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,31 +1,70 @@
-import { defineConfig, tierPresets } from 'sponsorkit'
+import fs from "node:fs/promises";
+
+import { BadgePreset, defineConfig, tierPresets } from "sponsorkit";
+
+const past: BadgePreset = {
+  avatar: {
+    size: 20,
+  },
+  boxWidth: 22,
+  boxHeight: 22,
+  container: {
+    sidePadding: 35,
+  },
+};
 
 export default defineConfig({
   tiers: [
     {
-      title: 'Backers',
+      title: "Backers",
       preset: tierPresets.small,
     },
     {
-      title: 'Sponsors',
+      title: "Sponsors",
       monthlyDollars: 25,
       preset: tierPresets.base,
     },
     {
-      title: 'Silver Sponsors',
+      title: "Silver Sponsors",
       monthlyDollars: 50,
       preset: tierPresets.medium,
     },
     {
-      title: 'Gold Sponsors',
+      title: "Gold Sponsors",
       monthlyDollars: 100,
       preset: tierPresets.large,
     },
     {
-      title: 'Platinum Sponsors',
+      title: "Platinum Sponsors",
       monthlyDollars: 500,
       preset: tierPresets.xl,
     },
+    {
+      title: "Past Sponsors",
+      monthlyDollars: -1,
+      preset: past,
+    },
   ],
-  includePastSponsors: true,
-})
+  async onSponsorsReady(sponsors) {
+    await fs.writeFile(
+      "sponsors.json",
+      JSON.stringify(
+        sponsors
+          .filter((i) => i.privacyLevel !== "PRIVATE")
+          .map((i) => {
+            return {
+              name: i.sponsor.name,
+              login: i.sponsor.login,
+              avatar: i.sponsor.avatarUrl,
+              amount: i.monthlyDollars,
+              link: i.sponsor.linkUrl || i.sponsor.websiteUrl,
+              org: i.sponsor.type === "Organization",
+            };
+          })
+          .sort((a, b) => b.amount - a.amount),
+        null,
+        2
+      )
+    );
+  },
+});
