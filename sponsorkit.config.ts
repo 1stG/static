560e564cd947bc6c64c7122a8bc30de798d10f19
JounQin <admin@1stg.me>
2025-03-03 13:55:31 +0800

    chore: bump deps

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,30 +1,30 @@
-import { defineConfig, presets } from 'sponsorkit'
+import { defineConfig, tierPresets } from 'sponsorkit'
 
 export default defineConfig({
   tiers: [
     {
       title: 'Backers',
-      preset: presets.small,
+      preset: tierPresets.small,
     },
     {
       title: 'Sponsors',
       monthlyDollars: 25,
-      preset: presets.base,
+      preset: tierPresets.base,
     },
     {
       title: 'Silver Sponsors',
       monthlyDollars: 50,
-      preset: presets.medium,
+      preset: tierPresets.medium,
     },
     {
       title: 'Gold Sponsors',
       monthlyDollars: 100,
-      preset: presets.large,
+      preset: tierPresets.large,
     },
     {
       title: 'Platinum Sponsors',
       monthlyDollars: 500,
-      preset: presets.xl,
+      preset: tierPresets.xl,
     },
   ]
 })
